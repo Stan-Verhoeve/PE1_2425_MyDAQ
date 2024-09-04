4237b52336d57acb3709dd635af58c7f0974a5d9
Stan <verhoeve@strw.leidenuniv.nl>
2024-09-04 16:18:11 +0200

    Base nidaqmx class

--- DAQ.py
+++ DAQ.py
@@ -1,40 +1,189 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Sep  4 14:41:56 2023
-
-@author: Stan_Verhoeve
-"""
-
-import nidaqmx as dx
 import numpy as np
+import nidaqmx as dx
+from scipy.signal import sawtooth, square
+import time
 
-class DAQ:
-    def __init__(self, samplerate):
-        self._samplerate = samplerate
+class MyDAQ:
+    def __init__(self):
+        self.__samplerate = None
+        self.__name = None
+    
+    @property
+    def samplerate(self):
+        return self.__samplerate
+    
+    @samplerate.setter
+    def samplerate(self, newSamplerate):
+        assert newSamplerate > 0 and type(newSamplerate) == int, "Samplerate should be a positive integer!"
+        self.__samplerate = newSamplerate
+    
+    @property
+    def name(self):
+        return self.__name
+    
+    @name.setter
+    def name(self, newName):
+        assert type(newName) == str, "Name should be a string!"
+        self.__name = newName
+    
+    def _addOutputChannels(self, task, channels):
+        """
+        Add output channels to the DAQ
+        """
+        assert self.name, "Name should be set first!"
+        
+        # Make sure channels can be iterated over
+        if isinstance(channels, str):
+            channels = [channels]
+            
+        # Iterate over all channels and add to task
+        for channel in channels:
+            if self.__name in channel:
+                task.ao_channels.add_ao_voltage_chan(channel)
+            else:
+                task.ao_channels.add_ao_voltage_chan(self.__name + f"{channel}")
+    
+    def _addInputChannels(self, task, channels):
+        """
+        Add input channels to the DAQ
+        """
+        assert type(self.name) is not type(None), "Name should be set first!"
+        
+        # Make sure channels can be iterated over
+        if isinstance(channels, str):
+            channels = [channels]
+            
+        # Iterate over all channels and add to task
+        for channel in channels:
+            task.ai_channels.add_ai_voltage_chan(channel)
+    
+    
+    def _configureChannelTimings(self, task, samples):
+        """
+        Set the correct timings for task based on number of samples
+        """
+        task.timing.cfg_samp_clk_timing(self.samplerate, 
+                                        sample_mode=dx.constants.AcquisitionType.FINITE,
+                                        samps_per_chan=samples)
+    
+    def _convertDurationToSamples(self, duration):
+        assert self.samplerate, "Samplerate should be set first!"
+        
+        samples = duration * self.samplerate
+        
+        # Round down to nearest integer
+        return int(samples)
+        
+    
+    def read(self, duration, *channels):
+        """
+        Read from user-specified channels for `duration` seconds
+        """
+        
+        # Convert duration to samples
+        samples = self._convertDurationToSamples(duration)
+        
+        # Create read task
+        with dx.Task("readOnly") as readTask:
+            self._addInputChannels(readTask, channels)
+            self._configureChannelTimings(readTask, samples)
+            
+            # Now read in data. Use WAIT_INFINITELY to assure ample reading time
+            data = readTask.read(number_of_samples_per_channel=samples, 
+                                 timeout=dx.constants.WAIT_INFINITELY)
+            
+            return np.asarray(data)
+    
+    def write(self, voltages, *channels):
+        """
+        Write `voltages` to user-specified channels. 
+        """
+        samples = max(voltages.shape)
+        
+        # Create write task
+        with dx.Task("writeOnly") as writeTask:
+            self._addOutputChannels(writeTask, channels)
+            self._configureChannelTimings(writeTask, samples)
+            
+            # Now write the data
+            writeTask.write(voltages, auto_start=True)
+            
+            # Wait for writing to finish
+            time.sleep(samples / self.samplerate + 1/1000)
+            writeTask.stop()
+    
+    def readwrite(self, voltages, readChannels, writeChannels):
+        samples = max(voltages.shape)
+        
+        with dx.Task("read") as readTask, dx.Task("write") as writeTask:
+            self._addOutputChannels(writeTask, writeChannels)
+            self._addInputChannels(readTask, readChannels)
+            
+            self._configureChannelTimings(writeTask, samples)
+            self._configureChannelTimings(readTask, samples)
+            
+            # Start writing. Since reading is a blocking function, there
+            # is no need to sleep and wait for writing to finish. 
+            writeTask.write(voltages, auto_start=True)
+            data = readTask.read(number_of_samples_per_channel=samples, timeout=dx.constants.WAIT_INFINITELY)
+            
+            return np.asarray(data)
+    
+    def generateWaveform(self, form, frequency, amplitude=1, phase=0, duration=1):
+        """
+        Geneate a waveform from the 4 basic wave parameters
+
+        Parameters
+        ----------
+        form : str
+            Type of waveform. 
+            Must be in ["sine", "square", "sawtooth", "isawtooth", "triangle"].
+        frequency : int or float
+            Frequency of the waveform.
+        amplitude : int or float, optional
+            Amplitude of the waveform in volts. The default is 1.
+        phase : int or float, optional
+            Phase of the waveform in degrees. The default is 0.
+        duration : int or float, optional
+            Duration of the waveform in seconds. The default is 1.
+
+        Returns
+        -------
+        timeArray : TYPE
+            DESCRIPTION.
+        wave : TYPE
+            DESCRIPTION.
+
+        """
+        assert self.samplerate, "Samplerate should be set first!"
+        
+        timeArray = MyDAQ.getTimeArray(duration, self.samplerate)
+        arg = 2*np.pi * frequency * timeArray + np.deg2rad(phase)
+        match form:
+            case "sine":
+                wave = amplitude * np.sin(arg)
+            case "square":
+                wave = amplitude * square(arg)
+            case "sawtooth":
+                wave = amplitude * sawtooth(arg)
+            case "isawtooth":
+                wave = amplitude * sawtooth(arg, width=0)
+            case "triangle":
+                wave = amplitude * sawtooth(arg, width=0.5)
+        return timeArray, wave
     
     @staticmethod
-    def addOutputChannels(task, channels):
-        if isinstance(channels, str):
-            DAQ.addOutputChannels(task, [channels])
-        else:
-            for chan in channels:
-                task.ao_channels.add_ao_voltage_chan(chan)
+    def getTimeArray(duration, samplerate):
+        return np.arange(1/samplerate, duration, 1/samplerate)
     
-    @staticmethod
-    def addInputChannels(task, channels):
-        if isinstance(channels, str):
-            DAQ.addInputChannels(task, [channels])
-        else:
-            for chan in channels:
-                task.ai_channels.add_ai_voltage_chan(chan)
-    
-    def setAcquisitionMode(self, task, sampleTime):
-        if isinstance(sampleTime, type(None)):
-            sampleMode = dx.constants.AcquisitionType.CONTINUOUS
-            task.timing.cfg_samp_clk_timing(self.samplerate, sample_mode=sampleMode)
-        else:
-            sampleMode = dx.constants.AcquisitionType.FINITE
-            
-            sampsPerChan = int(sampleTime * self.samplerate)
-            task.timing.cfg_samp_clk_timing(self.samplerate, sample_mode=sampleMode, samps_per_chan=sampsPerChan)
-            
+    def __str__(self):
+        """
+        Only used for pretty printing of class
+        E.g. using `print(MyDAQ)` will neatly print the most important
+        properties
+        """
+        title = f"MyDAQ instance"
+        
+        return title + f"\n{'=' * len(title)}" + \
+               f"\nBase name: {self.name}" + \
+               f"\nSample rate: {self.samplerate}"
