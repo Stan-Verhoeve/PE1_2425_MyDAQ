ffb92611a6a0a8df31517a47dd163f19da9b4b64
Stan Verhoeve <verhoeve@strw.leidenuniv.nl>
2024-12-11 12:45:44 +0100

    Added plot limits and new method

--- examples/spectrumMeasure.py
+++ examples/spectrumMeasure.py
@@ -30,11 +30,12 @@ signalOut, signalIn = daq.readwrite(white, ["AI0", "AI1"], "AO0")
 
 # Create Bode() instance and extract transfer function
 bode = Bode(daq.samplerate, signalOut, signalIn)
-freqs, H = bode.getTransfer(nperseg=int(white.size / 10))
+freqs, H = bode.getTransfer(nperseg=int(white.size / 50))
 
 # Plot transfer function
 plotBode(2*np.pi*freqs, np.abs(H), np.angle(H), 
-         xlim=(1, 1e5))
+         xlim=(10, 5e5), 
+         mag_ylim=(-80, 20))
 
 
 # =============================================================================
@@ -45,7 +46,7 @@ plotBode(2*np.pi*freqs, np.abs(H), np.angle(H),
 # =============================================================================
 
 # Measure over range of frequencies
-freqs = np.logspace(1, 4, 50)
+freqs = np.logspace(1, 5, 50)
 powers = np.zeros_like(freqs)
 phases = np.zeros_like(freqs)
 
@@ -68,4 +69,49 @@ for i, freq in enumerate(tqdm(freqs)):
     phases[i] = phase
 
 # Plot the bode plots
-plotBode(2 * np.pi * freqs, np.sqrt(powers), phases)
+plotBode(2 * np.pi * freqs, np.sqrt(powers), phases,
+         xlim=(10, 5e5), 
+         mag_ylim=(-80, 20))
+
+# =============================================================================
+# = There are two methods to measure the transfer function using SPAN         =
+# = The second method is slow but more accurate, and uses a sweep over        =
+# = different frequencies to determine the transfer function at each          =
+# = frequency individually                                                    =
+# = Here, we are using the `getTranfer` function of SPAN.bode, which          =
+# = returns the Welch power spectrum. We thus have to extract only at         =
+# = those points closest to the frequency of interest.                        =
+# = This and the previous method are equivalent.                              =
+# =============================================================================
+
+
+# Run simulation in the domain [1Hz, 100kHz)
+freqs = np.logspace(1, 5, 50)
+
+# Keep track of (simulated) magnitude and phase
+mags = np.zeros_like(freqs)
+phases = np.zeros_like(freqs)
+
+# Run simulation over range of frequencies
+for i, freq in enumerate(tqdm(freqs)):
+    # Input is simple sine, simulate output
+    timeArray, signalWrite = MyDAQ.generateWaveform("sine", daq.samplerate, frequency=freq)
+    signalOut, signalIn = daq.readwrite(signalWrite, ["AI0", "AI1"], "AO0")
+
+    # Create Bode() instance
+    bode = Bode(daq.samplerate, signalOut, signalIn)
+
+    # Get power and phase of freq, with a bandwidth delta=1
+    Hfreqs, H = bode.getTransfer(nperseg = int(signalIn.size) / 50)
+    index = np.argmin(abs(Hfreqs - freq))
+    
+    mag = abs(H)[index]
+    phase = np.angle(H)[index]
+    # Save power and phase of freq.
+    mags[i] = mag
+    phases[i] = phase
+
+# Plot the bode plots
+plotBode(2 * np.pi * freqs, mags, phases,
+         xlim=(10, 5e5), 
+         mag_ylim=(-80, 20))
